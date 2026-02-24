06b7f08349265914e229281a3e6c1e189ec42cfd
changl1112-ui <changl1112@gmail.com>
2026-02-24 14:32:48 -0800

    Add files via upload

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  i18n: {
+    locales: ['en', 'es', 'zh-cn'],
+    defaultLocale: 'en',
+    routing: {
+      prefixDefaultLocale: true
+    }
+  }
+});
